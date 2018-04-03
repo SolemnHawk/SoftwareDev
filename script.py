1ad5ae80b78a002856df6693168fa57799c553aa
sonrossy <tristesse0889@hotmail.com>
2018-04-03 12:21:37 -0400

    added instruction on how to run server after cloning project

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
 #a script that will configure a system .
+import os
+#after cloning the project cd to Game_server folder then run script
+#using pyhton3 script.py
+cmd1='python3 manage.py runserver'
+os.system(cmd1)
