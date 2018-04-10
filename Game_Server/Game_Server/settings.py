0288477d1ecf90d48c0ac3feb83aa73114acfb66
sonrossy <tristesse0889@hotmail.com>
2018-04-10 15:04:23 -0400

    not important

--- Game_Server/Game_Server/settings.py
+++ Game_Server/Game_Server/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django_reset_framework',
     'user',
     'django.contrib.staticfiles',
 ]
