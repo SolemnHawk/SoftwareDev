50dc5ce084ec82f367248fe9cbc5e6089e60b3ae
sonrossy <tristesse0889@hotmail.com>
2018-04-10 14:57:21 -0400

    added first method to user/view.py

--- Game_Server/user/views.py
+++ Game_Server/user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+import json
+
+# Create your views here.
+class HomePageView(APIView):
+    def get(self, request=None, uname="test", format=None):
+        data = { "user": uname, "key2": "value2" }
+        return HttpResponse(json.dumps(data))
+
 
 # Create your views here.
