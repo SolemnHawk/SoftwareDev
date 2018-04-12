76ccd8051c24f096d1324e038c9e807b6fdeeceb
sonrossy <tristesse0889@hotmail.com>
2018-04-11 21:31:17 -0400

    fixed users

--- Game_Server/user/views.py
+++ Game_Server/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from rest_framework.views import APIView
+from rest_framework.views import APIView
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -20,12 +20,19 @@ def index(request):
 
 
 
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
+import json
+from user.models import User
+from django.views.decorators.csrf import csrf_exempt
 
-def get(self,request=None, uname="test", format=None):
-    try:
-        found_user = User.objects.get(name=uname)
-    except ObjectDoesNotExist as e:
-        return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
+class HomePageView(APIView):
+    def get(self, request=None, uname="test", format=None):
+        try:
+            found_user = User.objects.get(name=uname)
+        except ObjectDoesNotExist as e:
+            return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
 
-    data = { "user": uname, "id": found_user.id}
-    return HttpResponse(json.dumps(data))
+        data = { "user": uname, "id": found_user.id }
+        return HttpResponse(json.dumps(data))
