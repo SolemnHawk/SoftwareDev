8c7670eecad84d64467ab8ad0d705df7565436ed
iancalhoun <ianmcalhoun@gmail.com>
2018-04-23 12:56:30 -0400

    Added PUT, POST can assign a subscription level

--- Game_Server/subscribe/views.py
+++ Game_Server/subscribe/views.py
@@ -9,10 +9,11 @@ import urllib.request
 
 class HomePageView(APIView):
 	@csrf_exempt
-	def create_or_retrieve(self, request=None, uname="test", format=None):
+	def create_or_retrieve(self, request=None, uname="test", subl = 0,format=None):
 		page = urllib.request.urlopen('http://localhost:8000/user/'+uname)
 		json_string = page.read()
 		parsed_json = json.loads(json_string)
+		
 		if request.method == "GET":
 			try:
 				found_id = parsed_json['id']
@@ -22,12 +23,25 @@ class HomePageView(APIView):
 			
 			data = {"id":user_id.id, "level":user_id.level}
 			return HttpResponse(json.dumps(data),status = 200)
+		
 		elif request.method == "POST":
 			try:
 				found_id = parsed_json['id']
+				user_id = Subscribe.objects.get(id = found_id)
 				return HttpResponse(json.dumps({"status":"AlreadyExists"}),status=403)
 			except ObjectDoesNotExist as e:
 				pass
 			u = Subscribe(id = found_id)
+			u = Subscribe(level = subl)
 			u.save()
 			return HttpResponse(json.dumps({"status":"Success"}))
+			
+		elif request.method == "PUT":
+			try:
+				found_id = parsed_json['id']
+				user = Subscribe.objects.get(id = found_id)
+				user.level = subl
+				user.save()
+				return HttpResponse(json.dumps({"status":"Success"}))
+			except ObjectDoesNotExist as e:
+				return HttpResponse(json.dumps({"status":"NoSuchUser"}))
