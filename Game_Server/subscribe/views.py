4a5eaf8278b367af1914c1eaa9e53b248e37c504
iancalhoun <ianmcalhoun@gmail.com>
2018-04-23 11:41:58 -0400

    change GET and POST to use username

--- Game_Server/subscribe/views.py
+++ Game_Server/subscribe/views.py
@@ -5,27 +5,32 @@ import json
 from subscribe.models import Subscribe
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
-
+import urllib.request
 
 class HomePageView(APIView):
 	@csrf_exempt
-	def create_or_retrieve(self, request=None, uid="test", format=None):
+	def create_or_retrieve(self, request=None, uname="test", format=None):
+		page = urllib.request.urlopen('http://localhost:8000/user/'+uname)
+		json_string = page.read()
+		parsed_json = json.loads(json_string)
 		if request.method == "GET":
 			try:
-				found_id = Subscribe.objects.get(id = uid)
+				found_id = parsed_json['id']
+				user_id = Subscribe.objects.get(id = found_id)
 			except ObjectDoesNotExist as e:
 				return HttpResponse(json.dumps({"status":"NoSuchID"}),status = 404)
 			
-			data = {"id":found_id.id, "level":found_id.level}
+			data = {"id":user_id.id, "level":user_id.level}
 			return HttpResponse(json.dumps(data),status = 200)
 		elif request.method == "POST":
 			try:
-				found_id = Subscribe.objects.get(id = uid)
+				found_id = parsed_json['id']
 				return HttpResponse(json.dumps({"status":"AlreadyExists"}),status=403)
 			except ObjectDoesNotExist as e:
 				pass
-			u = Subscribe(id = uid)
+			u = Subscribe(id = found_id)
 			u.save()
 			return HttpResponse(json.dumps({"status":"Success"}))
+		elif request.method == "PUT":
 			
 			
