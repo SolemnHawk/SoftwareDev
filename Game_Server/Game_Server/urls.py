34217a3fa8477a74d7789a26dda1d5e24a7a9a6e
Mark Hawker Jr <markhawkerap@gmail.com>
2018-04-24 23:34:42 -0400

    added put and changes to url.py

--- Game_Server/Game_Server/urls.py
+++ Game_Server/Game_Server/urls.py
@@ -1,4 +1,3 @@
-
 from user import views 
 from score import views
 from django.contrib import admin
@@ -10,19 +9,7 @@ from django.urls import path
 urlpatterns = [
     path('user/', uv.HomePageView().get),
     path('user/<str:uname>/', uv.HomePageView().get),
-    path('score/', scorev.HomePageView().create_or_retrieve),
-    path('score/<str:idval>/', scorev.HomePageView().create_or_retrieve),
+    	path('score/',scorev.HomePageView().create_or_retrieve),
+    	path('score/<str:uname>/',scorev.HomePageView().create_or_retrieve),
+    	path('score/<str:uname>/<str:scoreval>/', scorev.HomePageView().create_or_retrieve),
 ]
-
-'''urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', HomePageView().get),
-    path('user/<str:uname>/',HomePageView().get),
-    path('user/', views.index, name='index'),
-    #path('user/<str:uname>/', views.get),
-    path('score/',scorevHomePageView().create_or_retrieve(),
-    path('score/<str:idval>/',scorev.HomePageView().create_or_retrieve(),
-    path('score/', views.index, name='index'),
-    #path('score/<str:idval>/', views.create_or_retrieve),
-    
-]'''
--- Game_Server/score/views.py
+++ Game_Server/score/views.py
@@ -4,25 +4,43 @@ from django.core.exceptions import ObjectDoesNotExist
 import json
 from score.models import Score
 from django.views.decorators.csrf import csrf_exempt
+import urllib.request
 
 # Create your views here.
 class HomePageView(APIView):
     @csrf_exempt
-    def create_or_retrieve(self, request=None, idval="001", format=None):
+    def create_or_retrieve(self, request=None, uname="test",scoreval=0, format=None):
+        page=urllib.request.urlopen('http://localhost:8000/user/'+uname)
+        json_string=page.read()
+        parsed_kson=json.loads(json_string)
+
         if request.method =="GET":
             try:
-                    found_id = Score.objects.get(id=idval)
+                    found_id = parsed_json['id']
+                    user_id=Score.objects.get(id=found_id)
             except ObjectDoesNotExist as e:
                     return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
 
-            data = { "ID": idval, "Score": found_id.score }
+            data = { "ID": user_id, "Score": found_id.score }
             return HttpResponse(json.dumps(data))
+
         elif request.method == "POST":
             try:
-                found_id = Score.objects.get(id=idval)
+                found_id=parsed_json['id']
+                user_id = Score.objects.get(id=found_id)
                 return HttpResponse(json.dumps({"status":"AlreadyExists"}), status=403)
             except ObjectDoesNotExist as e:
                 pass
-            u = Score(id=idval)
+            u = Score(id=found_id)
             u.save()
             return HttpResponse(json.dumps({"status":"Success"}))
+
+        elif request.method =="PUT":
+            try:
+                found_id=parsed_json['id']
+                user=Score.objects.get(id=found_id)
+                user.score=scoreval
+                user.save()
+                return HttpResponse(json.dumps({"status":"Success"}))
+            except ObjectDoesNotExist as e:
+                return HttpResponse(json.dumps({"status":"NoSuchUser"}))
