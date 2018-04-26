0dbb0db23bac2ec3cceab67f758230305e6afd74
Mark Hawker Jr <markhawkerap@gmail.com>
2018-04-26 14:31:16 -0400

    Update urls.py

--- Game_Server/Game_Server/urls.py
+++ Game_Server/Game_Server/urls.py
@@ -8,10 +8,17 @@ from inventory import views as inuv
 from django.urls import path
 
 urlpatterns = [
-    path('user/', uv.HomePageView().create_or_retrieve),
-    path('user/<str:uname>/', uv.HomePageView().create_or_retrieve),
-    path('inventory/', inuv.HomePageView().create_or_retrieve),
-    path('inventory/<str:uname>/',inuv.HomePageView().create_or_retrieve),
-    path('inventory/<str:uname>/<str:description>/',inuv.HomePageView().create_or_retrieve),
-]
-
+-    path('user/', uv.HomePageView().get),
+-    path('user/<str:uname>/', uv.HomePageView().get),
++    path('user/', uv.HomePageView().create_or_retrieve),
++    path('user/<str:uname>/', uv.HomePageView().create_or_retrieve),
++	path('subscribe/', subv.HomePageView().create_or_retrieve),
++	path('subscribe/<str:uname>/',subv.HomePageView().create_or_retrieve),
++	path('subscribe/<str:uname>/<int:subl>/',subv.HomePageView().create_or_retrieve),
++    path('score/', scorev.HomePageView().create_or_retrieve),
++    path('score/<str:uname>/', scorev.HomePageView().create_or_retrieve),
++    path('score/<str:uname>/<str:scoreval>/',scorev.HomePageView().create_or_retrieve),
+     path('inventory/', inuv.HomePageView().create_or_retrieve),
+     path('inventory/<str:uname>/',inuv.HomePageView().create_or_retrieve),
+     path('inventory/<str:uname>/<str:description>/',inuv.HomePageView().create_or_retrieve)
+ ]
